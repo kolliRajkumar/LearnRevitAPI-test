c1d565d364e362f6b6bea0793f70e6a4ffac16eb
Rajkumar Kolli <rajkumar.ce@outlook.com>
2024-12-05 15:11:47 +0400

    ✨updated

--- LearnRevitAPI.tab/Dev.panel/ReuseCode.pushbutton/script.py
+++ LearnRevitAPI.tab/Dev.panel/ReuseCode.pushbutton/script.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 __title__ = "Reuse Code in pyRevit"
 __doc__ = """Version = 1.0
-Date    = 05.12.2024
+Date    = 05.12.2024----
 _____________________________________________________________________
 Description:
 Learn how to create reusable code with pyRevit.
 _____________________________________________________________________
 Author: Rajkumar Kolli"""
 
+from multiprocessing.managers import Namespace
 from sys import prefix
 
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
@@ -22,18 +23,9 @@ from Autodesk.Revit.DB import *
 #  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
 #==================================================
 from Autodesk.Revit.UI import UIDocument
+from unicodedata import category
+
 doc   = __revit__.ActiveUIDocument.Document #type : Document
 uidoc = __revit__.ActiveUIDocument          #type : UIDocument
 app   = __revit__.Application               #type : Application
 
-
-# Main
-from Snippets._selection import get_selected_elements
-
-sel_el = get_selected_elements()
-
-sel_walls = get_selected_elements([Wall, Floor])
-
-
-print(sel_el)
-print(sel_walls)

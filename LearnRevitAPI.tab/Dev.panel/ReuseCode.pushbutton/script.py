6582233b96891b4c7daeca898eecde7842454101
Rajkumar Kolli <rajkumar.ce@outlook.com>
2024-12-06 08:21:17 +0400

    📕code testing

--- LearnRevitAPI.tab/Dev.panel/ReuseCode.pushbutton/script.py
+++ LearnRevitAPI.tab/Dev.panel/ReuseCode.pushbutton/script.py
@@ -8,7 +8,6 @@ Learn how to create reusable code with pyRevit.
 _____________________________________________________________________
 Author: Rajkumar Kolli"""
 
-from multiprocessing.managers import Namespace
 from sys import prefix
 
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
@@ -23,9 +22,18 @@ from Autodesk.Revit.DB import *
 #  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
 #==================================================
 from Autodesk.Revit.UI import UIDocument
-from unicodedata import category
+#from unicodedata import category
+
+#from lib.Samples.FilteredElementCollector import collector
 
 doc   = __revit__.ActiveUIDocument.Document #type : Document
 uidoc = __revit__.ActiveUIDocument          #type : UIDocument
 app   = __revit__.Application               #type : Application
 
+#1️⃣Use ofcalss method to apply an elementclassfilter and find levels in the document
+
+collector = FilteredElementCollector(doc)
+levels    = collector.OfClass(Level).ToElements()
+
+for lvl in levels:
+    print(lvl,lvl.Name)
